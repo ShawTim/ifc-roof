689e293666d7da06c90e3ab24151b362e43c202c
Edditoria <edditoria@gmail.com>
2020-03-15 18:29:02 +0800

    Clean up; Standardize; Compile; Improve docs

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# ifc-roof
-香港交易廣場天台生成器
+## ifc-roof: 香港交易廣場天台生成器
 
 A simple static generator for a meme from HK famous drama 大時代.
 
@@ -8,8 +7,24 @@ See [demo](https://shawtim.github.io/ifc-roof/) on Github page.
 ![Introduction](https://shawtim.github.io/ifc-roof/intro.png)
 
 ### Installation
-`npm i`
 
-`npm run build`
+```bash
+npm i
+npm run build
+```
 
 All built files will then delivered to `/docs` folder. (the default Github page folder)
+
+### Test
+
+Open `/docs/index.html` in browser to preview, or:
+
+```bash
+npx http-server docs
+```
+
+Please make sure the text of location is readable, while spacing and size get close to original image:
+
+1. Mix of Chinese and English characters, e.g. "香港某 Hotel 騎樓"
+1. Reasonably long English name
+1. Some common punctuations, e.g. A.P.M.
--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+/* jshint esversion: 6 */
+
 import html2canvas from 'html2canvas';
-import FileSaver from "file-saver";
+import FileSaver from 'file-saver';
 
 // Full Width colon
 const colon = String.fromCharCode(65306);
 // Ideographic space as CJK block
 const space = String.fromCharCode(12288);
 
-// Convert "0123456789:" to "０１２３４５６７８９："
+// Convert '0123456789:' to '０１２３４５６７８９：'
 function convertToFullWidth(sentense) {
   let output = sentense.replace(/[0-9\:]/g, function(char) {
     return String.fromCharCode(char.charCodeAt(0) + 0xfee0);
@@ -24,17 +26,17 @@ if (typeof window !== 'undefined' && window !== null) {
 }
 
 $(function() {
-  $(".date-picker").datepicker({
-    dateFormat: "m d",
+  $('.date-picker').datepicker({
+    dateFormat: 'm d',
     onSelect: (date) => {
       const arr = date.split(' '); // Array<string>
       const fullWidthDate = convertToFullWidth(arr[0] + '月' + arr[1] + '日');
       console.log('Date is changed to:', fullWidthDate);
-      $(".date").text(fullWidthDate);
+      $('.date').text(fullWidthDate);
     }
   });
 
-  $(".time-picker input").timepicker({
+  $('.time-picker input').timepicker({
     timeFormat: 'h:mm p',
     defaultTime: '17',
     change: (time) => {
@@ -47,31 +49,31 @@ $(function() {
       minute = minute < 10 ? '0' + minute : '' + minute;
       const fullWidthTime = convertToFullWidth(head + hour + ':' + minute + amPm);
       console.log('Time is changed to:', fullWidthTime);
-      $(".time").text(fullWidthTime);
+      $('.time').text(fullWidthTime);
     }
   });
 
-  $(".location-picker input").keyup((e) => {
+  $('.location-picker input').keyup((e) => {
     const location = e.target.value;
-    $(".location").text(space + space + (location || '香港交易廣場天台'));
+    $('.location').text(space + space + (location || '香港交易廣場天台'));
   });
 
   const initDate = new Date();
   const initDateStr = convertToFullWidth(
     (initDate.getMonth() + 1) + '月' + initDate.getDate() + '日'
   );
-  $(".date").text(initDateStr);
+  $('.date').text(initDateStr);
   $('.time').text(space + convertToFullWidth('5:00') + 'ＰＭ');
   $('.location').text(space + space + '香港交易廣場天台');
 
-  $(".convert-button button").click((e) => {
+  $('.convert-button button').click((e) => {
     // Keeping scrollY is essential for html2canvas@1.0.0-rc.5
     // Ding Hai will find you if you remove it.
     const options = {
       scrollY: -window.scrollY
     };
-    html2canvas($(".image-container").get(0), options).then((canvas) => {
-      canvas.toBlob((blob) => FileSaver.saveAs(blob, "交易廣場天台.png"));
+    html2canvas($('.image-container').get(0), options).then((canvas) => {
+      canvas.toBlob((blob) => FileSaver.saveAs(blob, '交易廣場天台.png'));
     });
   });
 });
